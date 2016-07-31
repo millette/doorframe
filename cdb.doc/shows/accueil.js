bb2436ebfb4508385b9ab49e2c442f76f2762340
Robin Millette <robin@millette.info>
2016-07-31 13:15:51 -0400

    No comment.

--- cdb.doc/shows/accueil.js
+++ cdb.doc/shows/accueil.js
@@ -1,12 +1,5 @@
 function (doc, req) {
   templates = require('views/lib/templates')
 
-/*
-  if (req && req.userCtx && req.userCtx.name) {
-    body += '<form id="login"><input type="hidden" name="logout" value="logout"><input type="submit" value="logout"></form>'
-  } else {
-    body += '<form id="login"><input type="text" name="name" placeholder="username"></form>'
-  }
-*/
   return { body: templates.accueil({ user: req.userCtx }) }
 }
