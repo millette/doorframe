33c668091edeee226b88713be93e0633a9312d6f
Robin Millette <robin@millette.info>
2016-08-01 05:38:14 -0400

    Bugfix: test if 'prof' is in roles[].

--- cdb.doc/shows/prof.js
+++ cdb.doc/shows/prof.js
@@ -1,6 +1,6 @@
 function (doc, req) {
   const templates = require('views/lib/templates')
-  if (!req.userCtx.name || !req.userCtx.roles.indexOf('prof')) {
+  if (!req.userCtx.name || req.userCtx.roles.indexOf('prof') !== -1) {
     return { body: templates.prof({ user: req.userCtx }) }
   }
   return { code: 401, body: 'nope' }
