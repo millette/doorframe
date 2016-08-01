fe78f5a4f29be178bbab7cd1e7444a88b0f09cb2
Robin Millette <robin@millette.info>
2016-08-01 05:41:12 -0400

    Reverse logic to simplify code.

--- cdb.doc/shows/prof.js
+++ cdb.doc/shows/prof.js
@@ -1,7 +1,7 @@
 function (doc, req) {
   const templates = require('views/lib/templates')
-  if (!req.userCtx.name || req.userCtx.roles.indexOf('prof') !== -1) {
-    return { body: templates.prof({ user: req.userCtx }) }
+  if (req.userCtx.name && req.userCtx.roles.indexOf('prof') === -1) {
+    return { code: 401, body: 'nope' }
   }
-  return { code: 401, body: 'nope' }
+  return { body: templates.prof({ user: req.userCtx }) }
 }
