d66c634066a2c78f02724c835f37ab28f219a28f
Robin Millette <robin@millette.info>
2016-08-02 09:24:47 -0400

    Set charset to utf-8.

--- cdb.doc/lists/exams.js
+++ cdb.doc/lists/exams.js
@@ -2,13 +2,12 @@ function (head, req) {
   var row
   var s
   const templates = require('views/lib/templates')
-  start({ headers: { 'Content-Type': 'text/html' } })
+  start({ headers: { 'Content-Type': 'text/html; charset=utf-8' } })
   send(templates.htmlbegin())
   send('<h1><a href="/">Accueil</a></h1>')
   send('<h2>Les examens disponibles</h2>')
   send('<ul>')
   while((row = getRow())) {
-    log('row:' + JSON.stringify(row))
     s = '<li><a href="/exam/' + row.key[0] + '">' + row.key[0] + '</a> (' + row.value + ' questions)</li>'
     send(s)
   }
--- cdb.doc/lists/profdocs.js
+++ cdb.doc/lists/profdocs.js
@@ -2,7 +2,7 @@ function (head, req) {
   var row
   var s
   const templates = require('views/lib/templates')
-  start({ headers: { 'Content-Type': 'text/html' } })
+  start({ headers: { 'Content-Type': 'text/html; charset=utf-8' } })
   send(templates.htmlbegin())
   send('<h1><a href="/">Accueil</a></h1>')
   send('<ul>')
