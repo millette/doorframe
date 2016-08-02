b2d17b040e9f561be48f10fd597aaeb4dcc07ef6
Robin Millette <robin@millette.info>
2016-08-02 09:24:14 -0400

    Only show questions when allowed.

--- cdb.doc/lists/examexpanded.js
+++ cdb.doc/lists/examexpanded.js
@@ -2,6 +2,10 @@ function (head, req) {
   var row
   var s
   const templates = require('views/lib/templates')
+  if (!req.userCtx.name) {
+    start({ code: 401, headers: { 'Content-Type': 'text/html; charset=utf-8' } })
+    return send('Doit être loggué.')
+  }
   start({ headers: { 'Content-Type': 'text/html' } })
   send(templates.htmlbegin({ withmathjax: true }))
   send('<h1><a href="/">Accueil</a></h1>')
--- cdb.doc/shows/doc.js
+++ cdb.doc/shows/doc.js
@@ -1,10 +1,14 @@
 function (doc, req) {
-  const canEdit = req.userCtx.roles.indexOf('prof') !== -1
   const templates = require('views/lib/templates')
+
+  if (req.userCtx.roles.indexOf('prof') === -1) {
+    start({ code: 401, headers: { 'Content-Type': 'text/html; charset=utf-8' } })
+    return send('Doit être loggué en tant que prof.')
+  }
 
   return {
     body: templates.doc({
-      edit: canEdit && ('/' + req.requested_path.join('/') + '/edit'),
+      edit: '/' + req.requested_path.join('/') + '/edit',
       doc: doc,
       user: req.userCtx
     })
