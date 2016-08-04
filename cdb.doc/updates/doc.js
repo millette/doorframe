58616794c5f703dba4c9e740bfde07f58aae4379
Robin Millette <robin@millette.info>
2016-08-04 12:35:30 -0400

    Satisfy linter for couchdb update functions.

--- cdb.doc/updates/doc.js
+++ cdb.doc/updates/doc.js
@@ -1,50 +1,52 @@
-function (doc, req) {
-  if (!doc) {
-    doc = {
-      _id: req.form.id.trim(),
-      created_at: new Date().toISOString()
+(function () {
+  return function (doc, req) {
+    if (!doc) {
+      doc = {
+        _id: req.form.id.trim(),
+        created_at: new Date().toISOString()
+      }
     }
-  }
 
-  // doc.position = Math.min(Math.max(parseInt(req.form.position, 10), 1), 1000)
-  doc.question = req.form.question.trim()
-  doc.examens = req.form.examens.split(',')
-    .map(function (exam) { return exam.trim() })
-    .sort()
-  doc.choices = []
-  if (req.form.choice1) {
-    doc.choice = parseInt(req.form.answer, 10)
-    delete doc.answer
-    doc.choices.push(req.form.choice1.trim())
-    if (req.form.choice2) {
-      doc.choices.push(req.form.choice2.trim())
-      if (req.form.choice3) {
-        doc.choices.push(req.form.choice3.trim())
-        if (req.form.choice4) {
-          doc.choices.push(req.form.choice4.trim())
-          if (req.form.choice5) {
-            doc.choices.push(req.form.choice5.trim())
+    // doc.position = Math.min(Math.max(parseInt(req.form.position, 10), 1), 1000)
+    doc.question = req.form.question.trim()
+    doc.examens = req.form.examens.split(',')
+      .map(function (exam) { return exam.trim() })
+      .sort()
+    doc.choices = []
+    if (req.form.choice1) {
+      doc.choice = parseInt(req.form.answer, 10)
+      delete doc.answer
+      doc.choices.push(req.form.choice1.trim())
+      if (req.form.choice2) {
+        doc.choices.push(req.form.choice2.trim())
+        if (req.form.choice3) {
+          doc.choices.push(req.form.choice3.trim())
+          if (req.form.choice4) {
+            doc.choices.push(req.form.choice4.trim())
+            if (req.form.choice5) {
+              doc.choices.push(req.form.choice5.trim())
+            }
           }
         }
       }
-    }
-  } else {
-    doc.answer = req.form.answer.trim()
-    delete doc.choice
-  }
-
-  doc.updated_at = new Date().toISOString()
-
-  if (!doc.prof && req.userCtx.name) {
-    doc.prof = req.userCtx.name
-  }
-
-  if (req.userCtx.name) {
-    if (doc.editors && doc.editors.indexOf(req.userCtx.name) === -1) {
-      doc.editors.push(req.userCtx.name)
     } else {
-      doc.editors = [doc.prof]
+      doc.answer = req.form.answer.trim()
+      delete doc.choice
     }
+
+    doc.updated_at = new Date().toISOString()
+
+    if (!doc.prof && req.userCtx.name) {
+      doc.prof = req.userCtx.name
+    }
+
+    if (req.userCtx.name) {
+      if (doc.editors && doc.editors.indexOf(req.userCtx.name) === -1) {
+        doc.editors.push(req.userCtx.name)
+      } else {
+        doc.editors = [doc.prof]
+      }
+    }
+    return [doc, { code: 303, headers: { location: '/doc/' + doc._id } }]
   }
-  return [doc, { code: 303, headers: { location: '/doc/' + doc._id } }]
-}
+}())
--- cdb.doc/updates/position.js
+++ cdb.doc/updates/position.js
@@ -1,18 +1,20 @@
-function (doc, req) {
-  var i
-  if (!doc) {
-    return [null, { code: 401, body: 'What, no doc?' }]
-  }
+(function () {
+  return function (doc, req) {
+    var i
+    if (!doc) {
+      return [null, { code: 401, body: 'What, no doc?' }]
+    }
 
-  i = doc.examens.indexOf(req.query.exam)
-  if (i === -1) {
-    return [null, { code: 200, body: JSON.stringify({ error: 'Question not in exam.' }) }]
-  }
+    i = doc.examens.indexOf(req.query.exam)
+    if (i === -1) {
+      return [null, { code: 200, body: JSON.stringify({ error: 'Question not in exam.' }) }]
+    }
 
-  if (typeof doc.position !== 'object') {
-    doc.position = { }
-  }
+    if (typeof doc.position !== 'object') {
+      doc.position = { }
+    }
 
-  doc.position[req.query.exam] = Math.min(Math.max(parseInt(req.query.position, 10), 1), 1000)
-  return [doc, { code: 200, body: JSON.stringify({ ok: true }) }]
-}
+    doc.position[req.query.exam] = Math.min(Math.max(parseInt(req.query.position, 10), 1), 1000)
+    return [doc, { code: 200, body: JSON.stringify({ ok: true }) }]
+  }
+}())
